76d4aa591f3075030844046a9a5cc9df0231745b
yhnoh <tedgom19@gmail.com>
2023-05-12 23:51:50 +0900

    adapter pattern 알아보기

---
+++ adapter-pattern/src/test/java/com/example/adapterpattern/AdapterPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adapterpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
